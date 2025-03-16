d31d4cb8ddcb3aa31aaa49ef7a694e21b382bb2f
Riccardo Perissinotto <riccardoperissinotto2007@gmail.com>
2025-03-16 14:58:38 +0100

    Fixed a problem

--- src/test/java/Source/BankAccountTest.java
+++ src/test/java/Source/BankAccountTest.java
@@ -1,8 +1,8 @@
 package Source;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
   private BankAccount account;
--- src/test/java/Source/InvestmentTest.java
+++ src/test/java/Source/InvestmentTest.java
@@ -1,8 +1,8 @@
 package Source;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InvestmentTest {
   private Investment investment;
